b48468abb583fcbe4b2016f3e0f439e31596ad34
KristiyanMarinov <kristiqn_vm@abv.bg>
2020-12-05 15:51:53 +0200

    Firm

---
+++ SoftUniBasics/Firm/Program.cs
@@ -0,0 +1,21 @@
+﻿import java.util.Scanner;
+
+public class Firm
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        int hours = Integer.parseInt(scanner.nextLine());
+        int days = Integer.parseInt(scanner.nextLine());
+        int workers = Integer.parseInt(scanner.nextLine());
+        double workhours = Math.floor(0.9 * workers * 10 * days);
+        if (workhours >= hours)
+        {
+            System.out.printf("Yes!%.0f hours left.", Math.floor(workhours - hours));
+        }
+        else
+        {
+            System.out.printf("Not enough time!%.0f hours needed.", Math.floor(hours - workhours));
+        }
+    }
+}
