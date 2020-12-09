4f2f69bb8bdbcf36f048c8722f809215803cecc8
KristiyanMarinov <kristiqn_vm@abv.bg>
2020-12-09 16:17:26 +0200

    Java Basics

---
+++ SoftUniBasics/Simple Calculations/Pet Shop/Program.cs
@@ -0,0 +1,19 @@
+﻿import java.util.Scanner;
+
+public class PetShop
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        int dogs = Integer.parseInt(scanner.nextLine());
+        int other = Integer.parseInt(scanner.nextLine());
+        double razhod = 2.5 * dogs;
+        int otherdogs = 4 * other;
+        double all = razhod + otherdogs;
+        System.out.printf("%.2f", all);
+        System.out.println(" lv.");
+
+
+    }
+}
+
---
+++ SoftUniBasics/Simple Calculations/Yaard Greening/Program.cs
@@ -0,0 +1,21 @@
+﻿import java.util.Scanner;
+
+public class YardGreening
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        double area = Double.parseDouble(scanner.nextLine());
+        double price = area * 7.61;
+        double discount = 0.18 * price;
+        double endprice = price - discount;
+        System.out.print("The final price is: ");
+        System.out.printf("%.2f", endprice);
+        System.out.println(" lv.");
+
+        System.out.printf("The discount is: %.2f lv.", discount);
+
+
+
+    }
+}
