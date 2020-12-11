ce177d205640353b15ac95d00742d6876b47206c
KristiyanMarinov <kristiqn_vm@abv.bg>
2020-12-11 13:57:17 +0200

    Java basics

---
+++ SoftUniBasics/Conditional Statements/Excellent result/Program.cs
@@ -0,0 +1,15 @@
+﻿import java.util.Scanner;
+
+public class ExellentResult
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        double ocenka = Double.parseDouble(scanner.nextLine());
+        if (ocenka >= 5.5)
+        {
+            System.out.println("Excellent!");
+        }
+    }
+}
+
