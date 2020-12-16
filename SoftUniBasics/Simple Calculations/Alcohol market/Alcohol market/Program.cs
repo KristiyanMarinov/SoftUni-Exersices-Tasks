955178ff58a32afd784d9833eeb281c362c13366
KristiyanMarinov <kristiqn_vm@abv.bg>
2020-12-16 19:54:20 +0200

    Java basics

---
+++ SoftUniBasics/Simple Calculations/Alcohol market/Alcohol market/Program.cs
@@ -0,0 +1,21 @@
+﻿import java.util.Scanner;
+
+public class AlcoholMarket
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        double priceUiski = Double.parseDouble(scanner.nextLine());
+        double beerq = Double.parseDouble(scanner.nextLine());
+        double vinoq = Double.parseDouble(scanner.nextLine());
+        double rakiqq = Double.parseDouble(scanner.nextLine());
+        double uiskiq = Double.parseDouble(scanner.nextLine());
+
+        double pricerakiq = priceUiski * 0.5;
+        double pricevino = pricerakiq * 0.6;
+        double pricebeer = pricerakiq * 0.2;
+        System.out.printf("%.2f", priceUiski * uiskiq + beerq * pricebeer + vinoq * pricevino + rakiqq * pricerakiq);
+    }
+
+}
+
---
+++ SoftUniBasics/Simple Calculations/Tailoring Workshop/Tailoring Workshop/Program.cs
@@ -0,0 +1,25 @@
+﻿
+import java.util.Scanner;
+
+public class TailoringWorkshop
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        double tables = Double.parseDouble(scanner.nextLine());
+        double lenght = Double.parseDouble(scanner.nextLine());
+        double widht = Double.parseDouble(scanner.nextLine());
+        double areapokrivka = (lenght + 0.60) * (widht + 0.60);
+        double areakareta = (lenght / 2) * (lenght / 2);
+        double allareapokrivka = tables * areapokrivka;
+        double allareakareta = tables * areakareta;
+        double pricedollar = (allareakareta * 9) + (allareapokrivka * 7);
+        double pricelev = 1.85 * pricedollar;
+        System.out.printf("%.2f", pricedollar);
+        System.out.println(" USD");
+        System.out.printf("%.2f", pricelev);
+        System.out.println(" BGN");
+
+    }
+}
+
