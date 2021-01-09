af0cddd50303dc185d66031143fd5bfbafa2dbc1
KristiyanMarinov <kristiqn_vm@abv.bg>
2021-01-09 17:51:59 +0200

    C # Fundamentals

---
+++ C# Fundamentals/Arrays/Max sequence of equal/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO.IsolatedStorage;
+using System.Linq;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int theBest = 1;
+            int curr = 0;
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                int theLongest = 1;
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    if (input[i] == input[j])
+                    {
+                        theLongest += 1;
+                        if (theLongest >= theBest)
+                        {
+                            theBest = theLongest;
+                            curr = input[j];
+                        }
+
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+            }
+            for (int i = 1; i <= theBest; i++)
+            {
+                Console.Write(curr + " ");
+            }
+        }
+    }
+}
+
