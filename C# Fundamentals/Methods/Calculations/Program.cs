e893439235c452d9423b0b931b87aa67047f1631
KristiyanMarinov <kristiqn_vm@abv.bg>
2021-01-09 18:00:01 +0200

    C # Fundamentals

---
+++ C# Fundamentals/Methods/Calculations/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Threading.Tasks.Dataflow;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string action = Console.ReadLine();
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            switch (action)
+            {
+                case "add":
+                    Add(n1, n2);
+                    break;
+                case "multiply":
+                    Multiply(n1, n2);
+                    break;
+                case "substract":
+                    Subtract(n1, n2);
+                    break;
+                case "divide":
+                    Divide(n1, n2);
+                    break;
+
+            }
+
+
+            static void Subtract(int number1, int number2)
+            {
+                Console.WriteLine(number1 - number2);
+            }
+
+            static void Divide(int number1, int number2)
+            {
+                Console.WriteLine(number1 / number2);
+            }
+
+            static void Multiply(int number1, int number2)
+            {
+                Console.WriteLine(number1 * number2);
+            }
+
+            static void Add(int number1, int number2)
+            {
+                Console.WriteLine(number1 + number2);
+            }
+
+        }
+    }
+}
+
