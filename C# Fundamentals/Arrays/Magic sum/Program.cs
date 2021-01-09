ff3864dd1d66e06ee6e71605e3922098f8b8dc32
KristiyanMarinov <kristiqn_vm@abv.bg>
2021-01-09 17:53:14 +0200

    C # Fundamentals

---
+++ C# Fundamentals/Arrays/Magic sum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO.IsolatedStorage;
+using System.Linq;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int number = int.Parse(Console.ReadLine());
+            for (int i = 0; i < input.Length; i++)
+            {
+                int curr = input[i];
+                for (int j = i + 1; j < input.Length; j++)
+                {
+                    if ((curr + input[j]) == number)
+                    {
+                        Console.Write(curr + " " + input[j]);
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+    }
+}
+
