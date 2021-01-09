d18b387bca3881cfa3b492cd1ded9e9459c8a0e3
KristiyanMarinov <kristiqn_vm@abv.bg>
2021-01-09 17:58:30 +0200

    C # Fundamentals

---
+++ C# Fundamentals/Methods/Printing triangle/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Threading.Tasks.Dataflow;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            FirstHalf(n);
+            SecondHalf(n);
+
+            static void FirstHalf(int n)
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    for (int j = 1; j <= i; j++)
+                    {
+                        Console.Write(j + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            static void SecondHalf(int n)
+            {
+                for (int i = n - 1; i >= 1; i--)
+                {
+                    for (int j = 1; j <= i; j++)
+                    {
+                        Console.Write(j + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
+
---
+++ C# Fundamentals/Methods/Sign of integer numbers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Threading.Tasks.Dataflow;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            if (n > 0)
+            {
+                PositiveNumber(n);
+            }
+            else if (n == 0)
+            {
+                Zero(n);
+            }
+            else
+            {
+                NegativeNumber(n);
+            }
+
+            static void PositiveNumber(int number)
+            {
+                Console.WriteLine($"The number {number} is positive.");
+            }
+            static void Zero(int number)
+            {
+                Console.WriteLine($"The number {number} is zero.");
+            }
+            static void NegativeNumber(int number)
+            {
+                Console.WriteLine($"The number {number} is negative.");
+            }
+        }
+    }
+}
+
