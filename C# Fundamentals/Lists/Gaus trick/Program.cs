12a88ef009377ac2f5afaabe9c9b108a9eb4fbe4
KristiyanMarinov <kristiqn_vm@abv.bg>
2021-01-13 19:41:37 +0200

    C # fundamentals

---
+++ C# Fundamentals/Lists/Gaus trick/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FactorialDivision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+            int number = input.Count / 2;
+            for (int i = 0; i < number; i++)
+            {
+                input[i] += input[input.Count - 1];
+                input.Remove(input[input.Count - 1]);
+            }
+            Console.WriteLine(string.Join(" ", input));
+        }
+    }
+}
+
