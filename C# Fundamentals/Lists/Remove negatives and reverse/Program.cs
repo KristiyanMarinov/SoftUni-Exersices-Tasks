d306461894d73f8e197a4a9955af9a4f96004cfd
KristiyanMarinov <kristiqn_vm@abv.bg>
2021-01-13 19:46:49 +0200

    C # fundamentals

---
+++ C# Fundamentals/Lists/Remove negatives and reverse/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FactorialDivision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> input = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                if (input[i] < 0)
+                {
+                    input.RemoveAt(i);
+                    i -= 1;
+                }
+            }
+            if (input.Count > 0)
+
+            {
+                input.Reverse();
+                Console.WriteLine(string.Join(" ", input));
+            }
+            else
+            {
+                Console.WriteLine("empty");
+            }
+
+
+        }
+    }
+}
