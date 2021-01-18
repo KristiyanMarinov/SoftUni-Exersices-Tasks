1917fe9b861daf806677e70f5e5005368e068880
KristiyanMarinov <kristiqn_vm@abv.bg>
2021-01-18 20:29:43 +0200

    C # fundamentals

---
+++ C# Fundamentals/Lists/Bomb number/Program.cs
@@ -0,0 +1,39 @@
+﻿usiusing System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FactorialDivision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> input = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            int[] command = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            for (int i = 0; i < input.Count; i++)
+            {
+                if (input[i] == command[0])
+                {
+
+                    int startIndex = i - command[1];
+                    int endIndex = i + command[1];
+                    if (startIndex < 0)
+                    {
+                        startIndex = 0;
+                    }
+                    if (endIndex > input.Count - 1)
+                    {
+                        endIndex = input.Count - 1;
+                    }
+                    int count = endIndex - startIndex + 1;
+                    input.RemoveRange(startIndex, count);
+                    i = startIndex - 1;
+                }
+            }
+            int sum = input.Sum();
+
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ C# Fundamentals/Lists/Cards game/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FactorialDivision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> input1 = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            List<int> input2 = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+            while (input1.Count != 0 && input2.Count != 0)
+            {
+                int cur1 = input1[0];
+                int cur2 = input2[0];
+
+                if (cur1 > cur2)
+                {
+                    input1.Add(cur1);
+                    input1.Add(cur2);
+
+                }
+                else if (cur2 > cur1)
+                {
+                    input2.Add(cur1);
+                    input2.Add(cur2);
+
+
+                }
+
+                input1.RemoveAt(0);
+                input2.RemoveAt(0);
+
+
+            }
+            if (input2.Count == 0)
+            {
+                int sum = input1.Sum();
+                Console.WriteLine($"First player wins! Sum: {sum}");
+            }
+            else
+            {
+                int sum = input2.Sum();
+                Console.WriteLine($"Second player wins! Sum: {sum}");
+            }
+        }
+    }
+}
