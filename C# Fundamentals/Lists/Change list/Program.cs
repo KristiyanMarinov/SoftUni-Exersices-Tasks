7d3b15249ad6e657af4c5110669efeb86ed308f4
KristiyanMarinov <kristiqn_vm@abv.bg>
2021-01-18 20:21:47 +0200

    C # fundamentals

---
+++ C# Fundamentals/Lists/Change list/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FactorialDivision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> input = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+            string[] command = Console.ReadLine().Split(" ");
+
+            while (command[0] != "end")
+            {
+                if (command[0].Equals("Delete"))
+                {
+                    input.RemoveAll(g => g == int.Parse(command[1]));
+                }
+                else
+                {
+                    input.Insert(int.Parse(command[2]), int.Parse(command[1]));
+                }
+                command = Console.ReadLine().Split(" ");
+            }
+            Console.WriteLine(string.Join(" ", input));
+        }
+    }
+}
