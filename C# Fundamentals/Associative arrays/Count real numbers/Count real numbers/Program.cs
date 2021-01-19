9fab52ee58bc2be713f970074a60d3cb57edcab3
KristiyanMarinov <kristiqn_vm@abv.bg>
2021-01-19 20:21:38 +0200

    C # fundamentals

---
+++ C# Fundamentals/Associative arrays/Count real numbers/Count real numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Globalization;
+using System.Linq;
+
+class HolidaysBetweenTwoDates
+{
+    static void Main()
+    {
+        double[] numbers = Console.ReadLine().Split().Select(double.Parse).ToArray();
+        SortedDictionary<double, int> myDictionary = new SortedDictionary<double, int>();
+
+        foreach (var item in numbers)
+        {
+            if (myDictionary.ContainsKey(item))
+            {
+                myDictionary[item] += 1;
+            }
+            else
+            {
+                myDictionary.Add(item, 1);
+            }
+        }
+        foreach (var item in myDictionary)
+        {
+            Console.WriteLine($"{item.Key} -> {item.Value}");
+        }
+    }
+}
