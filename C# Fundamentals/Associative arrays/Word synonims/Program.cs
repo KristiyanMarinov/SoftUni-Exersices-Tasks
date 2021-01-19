551fc2069ad186db3ccf976bb1fe9c3efa4857ce
KristiyanMarinov <kristiqn_vm@abv.bg>
2021-01-19 20:22:58 +0200

    C # fundamentals

---
+++ C# Fundamentals/Associative arrays/Word synonims/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Globalization;
+using System.Linq;
+
+class HolidaysBetweenTwoDates
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        Dictionary<string, List<string>> adj = new Dictionary<string, List<string>>();
+
+        for (int i = 1; i <= n; i++)
+        {
+            string word = Console.ReadLine();
+            string adjective = Console.ReadLine();
+
+            if (adj.ContainsKey(word))
+            {
+                adj[word].Add(adjective);
+            }
+            else
+            {
+                adj.Add(word, new List<string>() { adjective });
+            }
+        }
+        foreach (var word in adj)
+        {
+            Console.WriteLine($"{word.Key} - {string.Join(", ", word.Value)}");
+        }
+
+    }
+}
