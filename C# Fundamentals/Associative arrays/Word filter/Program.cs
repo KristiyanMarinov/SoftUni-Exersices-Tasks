a9178c1f9fec76d78efd9440932aa8899e9a7339
KristiyanMarinov <kristiqn_vm@abv.bg>
2021-01-19 20:27:32 +0200

    C # fundamentals

---
+++ C# Fundamentals/Associative arrays/Word filter/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Globalization;
+using System.Linq;
+
+class HolidaysBetweenTwoDates
+{
+    static void Main()
+    {
+
+        string[] input = Console.ReadLine().Split();
+        input.Where(x => x.Length % 2 == 0).ToList().ForEach(x => Console.WriteLine(x));
+
+    }
+}
