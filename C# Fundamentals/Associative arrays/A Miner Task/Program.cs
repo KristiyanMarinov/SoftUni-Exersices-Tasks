f05cc8f4d6d49f2017fa65c0c01df6ef15bcc310
KristiyanMarinov <kristiqn_vm@abv.bg>
2021-01-23 20:56:15 +0200

    C # fundamentals

---
+++ C# Fundamentals/Associative arrays/A Miner Task/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Globalization;
+using System.Linq;
+
+class HolidaysBetweenTwoDates
+{
+    static void Main()
+    {
+
+        Dictionary<string, int> resourses = new Dictionary<string, int>();
+        string input = Console.ReadLine();
+
+
+        while (input != "stop")
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if (!resourses.ContainsKey(input))
+            {
+                resourses.Add(input, number);
+            }
+            else
+            {
+                resourses[input] += number;
+            }
+            input = Console.ReadLine();
+
+        }
+        foreach (var item in resourses)
+        {
+            Console.WriteLine($"{item.Key} -> {item.Value}");
+        }
+    }
+}
