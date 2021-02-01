ccc9b6e6300c4123df9ddb25c24fec1a04ca5006
KristiyanMarinov <kristiqn_vm@abv.bg>
2021-02-01 22:11:12 +0200

    C # fundamentals

---
+++ C# Fundamentals/Associative arrays/StudentAcademy/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+class HolidaysBetweenTwoDates
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        Dictionary<string, List<double>> grades = new Dictionary<string, List<double>>();
+
+        for (int i = 0; i < n; i++)
+        {
+            string studentName = Console.ReadLine();
+            double studentGrade = double.Parse(Console.ReadLine());
+
+            if (!grades.ContainsKey(studentName))
+            {
+                grades.Add(studentName, new List<double> { studentGrade });
+            }
+            else
+            {
+                grades[studentName].Add(studentGrade);
+            }
+        }
+        List<double> average = new List<double>();
+        //Dictionary<string,List<double>> filteredDictionary = grades.Where(x => x.Value.Average()>=4.5).Select(x.Value.Average()).ToDictionary(x=>x.Key,y=>y.Value);
+        foreach (var item in grades.OrderByDescending(x => x.Value.Average()))
+        {
+            if (item.Value.Average() >= 4.5)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value.Average():f2}");
+            }
+        }
+    }
+}
