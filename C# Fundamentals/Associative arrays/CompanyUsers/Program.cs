cba10cb86096f708525d8ac0a97dda2252a703ec
KristiyanMarinov <kristiqn_vm@abv.bg>
2021-02-01 22:08:44 +0200

    C # fundamentals

---
+++ C# Fundamentals/Associative arrays/CompanyUsers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+class HolidaysBetweenTwoDates
+{
+    static void Main()
+    {
+        string command = Console.ReadLine();
+
+        SortedDictionary<string, List<string>> companies = new SortedDictionary<string, List<string>>();
+        while (command != "End")
+        {
+            string[] input = command.Split(" -> ");
+            string companyName = input[0];
+            string employeeName = input[1];
+
+            if (!companies.ContainsKey(companyName))
+            {
+                companies.Add(companyName, new List<string> { employeeName });
+            }
+            else
+            {
+                if (!companies[companyName].Contains(employeeName))
+                {
+                    companies[companyName].Add(employeeName);
+                }
+            }
+            command = Console.ReadLine();
+        }
+        foreach (var item in companies)
+        {
+            Console.WriteLine(item.Key);
+            foreach (var id in item.Value)
+            {
+                Console.WriteLine($"-- {id}");
+            }
+        }
+    }
+}
