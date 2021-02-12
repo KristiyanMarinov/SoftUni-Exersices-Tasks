b1a7e987eb1e0aca16ad8dc55ba0e444e91673f0
KristiyanMarinov <kristiqn_vm@abv.bg>
2021-02-12 22:12:36 +0200

    C # fundamentals

---
+++ C# Fundamentals/Text processing/TextFilter/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+class HolidaysBetweenTwoDates
+{
+    static void Main(string[] args)
+
+    {
+        string[] bannedWords = Console.ReadLine().Split(", ");
+        string text = Console.ReadLine();
+
+        foreach (var word in bannedWords)
+        {
+            string replace = new string('*', word.Length);
+            text = text.Replace(word, replace);
+        }
+        Console.WriteLine(text);
+    }
+}
