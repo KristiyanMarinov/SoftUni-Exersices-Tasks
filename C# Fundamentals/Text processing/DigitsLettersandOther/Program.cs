3e8d0af06c431203cc74bbb4140fe9d0e56df7a7
KristiyanMarinov <kristiqn_vm@abv.bg>
2021-02-12 22:14:19 +0200

    C # fundamentals

---
+++ C# Fundamentals/Text processing/DigitsLettersandOther/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+class HolidaysBetweenTwoDates
+{
+    static void Main(string[] args)
+
+    {
+        char[] input = Console.ReadLine().ToCharArray();
+        string digits = string.Empty;
+        string letters = string.Empty;
+        string symbols = string.Empty;
+
+        foreach (var item in input)
+        {
+            if (char.IsDigit(item))
+            {
+                digits += item;
+            }
+            else if (char.IsLetter(item))
+            {
+                letters += item;
+            }
+            else
+            {
+                symbols += item;
+            }
+
+        }
+        Console.WriteLine(digits);
+        Console.WriteLine(letters);
+        Console.WriteLine(symbols);
+
+
+
+    }
+}
