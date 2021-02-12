dd9369b455979b04aa2310202c18f23f1c3f3691
KristiyanMarinov <kristiqn_vm@abv.bg>
2021-02-12 22:06:22 +0200

    C # fundamentals

---
+++ C# Fundamentals/Text processing/ReverseStrings/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+class HolidaysBetweenTwoDates
+{
+    static void Main()
+
+    {
+        string command = Console.ReadLine();
+
+        while (command != "end")
+        {
+            string newWord = "";
+            for (int i = command.Length - 1; i >= 0; i--)
+            {
+                newWord += command[i];
+            }
+            Console.WriteLine($"{command} = {newWord}");
+            command = Console.ReadLine();
+        }
+
+    }
+}
