c37d131f8928ce0743bcc6e5752e3ee73ef2f6da
KristiyanMarinov <kristiqn_vm@abv.bg>
2021-02-12 22:11:05 +0200

    C # fundamentals

---
+++ C# Fundamentals/Text processing/Substring/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+class HolidaysBetweenTwoDates
+{
+    static void Main(string[] args)
+
+    {
+        string word = Console.ReadLine();
+        string newWord = Console.ReadLine();
+        int index = newWord.IndexOf(word);
+        while (index != -1)
+        {
+
+            newWord = newWord.Remove(index, word.Length);
+            index = newWord.IndexOf(word);
+        }
+
+        Console.WriteLine(newWord);
+    }
+}
