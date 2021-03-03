3f94e06a7f6190ec0dff3e7cf88a393e68df9a8b
KristiyanMarinov <kristiqn_vm@abv.bg>
2021-03-03 20:27:47 +0200

    C # Fundamentals

---
+++ C# Fundamentals/Text processing/CaesarCipher/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Numerics;
+
+namespace DataTypesAndViariables___Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = Console.ReadLine();
+            string result = string.Empty;
+            for (int i = 0; i < fileName.Length; i++)
+            {
+                char curr = fileName[i];
+                char rename = (char)(curr + 3);
+                result += rename;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
