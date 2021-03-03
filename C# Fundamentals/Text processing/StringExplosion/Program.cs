12930df57b698bd48dce8cf24e8d380b4a517957
KristiyanMarinov <kristiqn_vm@abv.bg>
2021-03-03 20:30:33 +0200

    C # Fundamentals

---
+++ C# Fundamentals/Text processing/StringExplosion/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Numerics;
+using System.Text;
+
+namespace DataTypesAndViariables___Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            StringBuilder result = new StringBuilder(input);
+            int currBombPower = 0;
+            int lastIndex = input.LastIndexOf('>');
+            for (int i = 0; i < result.Length - 1; i++)
+            {
+                if (result[i] == '>')
+                {
+                    currBombPower += int.Parse((result[i + 1]).ToString());
+                    continue;
+                }
+                if (currBombPower > 0)
+                {
+                    result.Remove(i, 1);
+                    i--;
+                    currBombPower--;
+                }
+            }
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
+
