ef5a4636bb6c226c00cc4ca4bda61fb684087dca
KristiyanMarinov <kristiqn_vm@abv.bg>
2021-03-03 20:26:25 +0200

    C # Fundamentals

---
+++ C# Fundamentals/Text processing/ExtractFile/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Numerics;
+
+namespace DataTypesAndViariables___Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = Console.ReadLine();
+            int lastindex = fileName.LastIndexOf("\\");
+            int lastIndexOfExtension = fileName.LastIndexOf(".");
+            string result = fileName.Substring(lastindex + 1, lastIndexOfExtension - lastindex - 1);
+            string extension = fileName.Substring(lastIndexOfExtension + 1);
+            Console.WriteLine($"File name: {result}");
+            Console.WriteLine($"File extension: {extension}");
+        }
+    }
+}
