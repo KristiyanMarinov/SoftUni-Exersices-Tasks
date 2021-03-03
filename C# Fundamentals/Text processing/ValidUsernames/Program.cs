da1768ff8bb524ebe380dd7537672608cc5a14fb
KristiyanMarinov <kristiqn_vm@abv.bg>
2021-03-03 20:22:57 +0200

    C# Fundamentals

---
+++ C# Fundamentals/Text processing/ValidUsernames/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+class HolidaysBetweenTwoDates
+{
+    static void Main(string[] args)
+
+    {
+        string[] input = Console.ReadLine().Split(", ");
+
+        foreach (var item in input)
+        {
+            if (item.Length <= 16 && item.Length >= 3)
+            {
+                bool isTrue = true;
+                foreach (var symbol in item)
+                {
+                    if (!(char.IsLetterOrDigit(symbol) || symbol == '-' || symbol == '_'))
+                    {
+                        isTrue = false;
+                        break;
+                    }
+                }
+                if (isTrue)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
