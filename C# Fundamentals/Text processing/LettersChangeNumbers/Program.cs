69572e6b17d44e9794cd0499b73fcaf302a39f49
KristiyanMarinov <kristiqn_vm@abv.bg>
2021-03-03 20:32:39 +0200

    C # Fundamentals

---
+++ C# Fundamentals/Text processing/LettersChangeNumbers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Numerics;
+using System.Text;
+
+namespace DataTypesAndViariables___Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            double result = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string currString = input[i];
+                double currNumber = double.Parse(currString.Substring(1, currString.Length - 2));
+                if (char.IsUpper(currString[0]))
+                {
+                    currNumber = currNumber / (currString[0] - 64);
+                }
+                else
+                {
+                    currNumber = currNumber * (currString[0] - 96);
+                }
+
+
+                if (char.IsUpper(currString[currString.Length - 1]))
+                {
+                    currNumber = currNumber - (currString[currString.Length - 1] - 64);
+                }
+                else
+                {
+                    currNumber += (currString[currString.Length - 1] - 96);
+                }
+                result += currNumber;
+            }
+            Console.WriteLine($"{result:f2}");
+        }
+    }
+}
